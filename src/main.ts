612d28bb180f89c67864f04f924a7aaebcc22746
OcaenEyes <1450136519@qq.com>
2023-10-28 21:39:02 +0800

    update 重做

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,27 @@
  * @Author: OCEAN.GZY
  * @Date: 2023-10-22 13:04:44
  * @LastEditors: OCEAN.GZY
- * @LastEditTime: 2023-10-28 15:37:50
- * @FilePath: \ocean-doctool\src\main.ts
+ * @LastEditTime: 2023-10-28 21:38:24
+ * @FilePath: /ocean-doctool/src/main.ts
  * @Description: 注释信息
  */
 import { createApp } from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faCircle,faSquare,faSquareCaretDown } from '@fortawesome/free-regular-svg-icons'
+import { faCircle, faSquare, faSquareCaretDown } from '@fortawesome/free-regular-svg-icons'
 import Antd from 'ant-design-vue'
 
 // import './style.css'
 import './reset.less'
 import App from './App.vue'
-import  router  from "./router"
+import router from "./router"
+// import store from "./store"
 
-library.add(faCircle,faSquare,faSquareCaretDown)
+library.add(faCircle, faSquare, faSquareCaretDown)
 
 createApp(App)
     .use(router)
     .use(Antd)
+    // .use(store)
     .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,30 @@
  * @Author: OCEAN.GZY
  * @Date: 2023-10-22 09:44:32
  * @LastEditors: OCEAN.GZY
- * @LastEditTime: 2023-10-28 17:29:24
+ * @LastEditTime: 2023-10-28 21:04:55
  * @FilePath: /ocean-doctool/vite.config.ts
  * @Description: 注释信息
  */
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue()
-  ],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, 'src'), // 路径别名
+export default defineConfig(({ command, mode }) => {
+  // 根据当前工作目录中的 `mode` 加载 .env 文件
+  // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
+  loadEnv(mode, process.cwd())
+  return {
+
+    plugins: [
+      vue()
+    ],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, 'src'), // 路径别名
+      },
+      extensions: ['.js', '.json', '.ts', '.vue'] // 使用路径别名时想要省略的后缀名，可以自己 增减
     },
-    extensions: ['.js', '.json', '.ts', '.vue'] // 使用路径别名时想要省略的后缀名，可以自己 增减
-  },
-  optimizeDeps: { exclude: ["fsevents"] }
+    optimizeDeps: { exclude: ["fsevents"] }
+  };
 })
- 
